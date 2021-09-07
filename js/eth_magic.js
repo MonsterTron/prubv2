f13053ad628b758117c7773c8284cd7c1d891ee1
MonsterTron <73758495+MonsterTron@users.noreply.github.com>
2021-09-07 20:38:50 +0200

    Update eth_magic.js

--- js/eth_magic.js
+++ js/eth_magic.js
@@ -5,7 +5,7 @@
     });
 
       // CONTRACT ADDRESSS! 
-      const contract_address = "0x631CdDd1E7c94c2C602374B0D306cd2FF3839d55";
+      const contract_address = "0xd2b59C177aB438A31a8181809F46506d4319B78D";
       // USER ACCOUNT!
       var account =  web3.eth.accounts[0];
 
